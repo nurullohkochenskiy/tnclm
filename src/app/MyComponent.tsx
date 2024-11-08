28edb6c41e7d7b95df5b1ada63430683d9405048
nurullohkochenskiy <serioskiller2021@gmail.com>
2024-11-08 21:08:19 +0500

    123

---
+++ src/app/MyComponent.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react';
+
+export default function MyComponent() {
+  // Set the type of tgWebAppVersion to 'string | null'
+  const [tgWebAppVersion, setTgWebAppVersion] = useState<string | null>(null);
+
+  useEffect(() => {
+    const hash = window.location.hash.slice(1);
+    const params = new URLSearchParams(hash);
+    setTgWebAppVersion(params.get('tgWebAppVersion'));
+  }, []);
+
+  return (
+    <div>
+      <p>TG Web App Version: {tgWebAppVersion}</p>
+    </div>
+  );
+}
